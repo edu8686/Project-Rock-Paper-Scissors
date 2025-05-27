e088753733164345cdae1f35f25c9d078771d23e
Eduardo <edu.denegri@gmail.com>
2025-05-27 15:47:42 -0300

    UI added.JS modifiedto implement it.

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+const btnRock = document.querySelector("#rock");
+const btnPaper = document.querySelector("#paper");
+const btnScissors = document.querySelector("#scissors");
+const roundResult = document.querySelector("#round-result");
+const score = document.querySelector("#score");
+const matchResult = document.querySelector("#match-result");
+const resetButton = document.querySelector("#reset");
+
+let humanScore = 0;
+let computerScore = 0;
+let rounds = 1;
+
+const parrafoRound = document.createElement("p");
+const resultadoRound = document.createElement("p");
+const resultadoAcum = document.createElement("p");
+const resultadoMatch = document.createElement("p");
+
+btnRock.addEventListener("click", (e) => {
+  const eleccion = e.target.id;
+  playRound(eleccion);
+  rounds += 1;
+});
+
+btnPaper.addEventListener("click", (e) => {
+  const eleccion = e.target.id;
+  playRound(eleccion);
+  rounds += 1;
+});
+
+btnScissors.addEventListener("click", (e) => {
+  const eleccion = e.target.id;
+  playRound(eleccion);
+  rounds += 1;
+});
+
+resetButton.addEventListener('click', reset);
+
 function getComputerChoice() {
   let randomNum = 4;
   while (randomNum > 2) {
@@ -12,52 +49,76 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  let eleccion = prompt(
-    "Input your choice: rock, paper o scissors"
-  ).toLowerCase();
-  return eleccion;
-}
-
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
-    if (
-      (humanChoice === "rock" && computerChoice === "scissors") ||
-      (humanChoice === "paper" && computerChoice === "rock") ||
-      (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
-      humanScore += 1;
-    } else if (humanChoice === computerChoice) {
-    } else {
-      computerScore += 1;
-    }
+function playRound(humanChoice) {
+  if(resetButton.classList.contains("hidden")) {
+    resetButton.classList.remove("hidden");
   }
-
-  for (let i = 1; i <=  5; i++) {
-    const humanSelection = getHumanChoice();
-    console.log(`Ronda ${i}: ${humanSelection}`)
-    const computerSelection = getComputerChoice();
-    console.log(`Ronda ${i}: ${computerSelection}`)
-    playRound(humanSelection, computerSelection)
-    console.log(humanScore);
-    console.log(computerScore)
-  }
-
-  if(humanScore > computerScore) {
-    return "You won the match!!"
-  } else if (computerScore > humanScore) {
-    return "Computer wins!!"
+  const computerChoice = getComputerChoice();
+  console.log("PlayRound ejecutada");
+  console.log("Human choice: ", humanChoice);
+  console.log("Computer choice: ", computerChoice);
+  let mensaje = "";
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log("You win!");
+    humanScore += 1;
+    mensaje = `You chose ${humanChoice}. Computer chose ${computerChoice}. You won!`;
+  } else if (humanChoice === computerChoice) {
+    mostrarResultados("Tie");
+    console.log("It's a tie");
+    mensaje = `You chose ${humanChoice}. Computer chose ${computerChoice}. It's a tie!`;
   } else {
-    return "No one wins. It's a tie!!"
+    console.log("Computer wins!");
+    computerScore += 1;
+    mensaje = `You chose ${humanChoice}. Computer chose ${computerChoice}. Computer won!`;
   }
-
+  return mostrarResultados(mensaje);
 }
 
+function mostrarResultados(resultado) {
+  console.log("Resultado: ", resultado);
+  console.log(humanScore);
+  console.log(computerScore);
+  console.log("Round: ", rounds);
+  // roundResult.classList.remove("hidden");
+  // score.classList.remove("hidden")
+  parrafoRound.textContent = `Round: ${rounds}`;
+  parrafoRound.style.fontSize = "1.5rem"
+  resultadoRound.textContent = `Resultado: ${resultado}`;
+  resultadoRound.style.fontSize = "1.5rem"
+  resultadoAcum.textContent = `Your score: ${humanScore}. Computer's score: ${computerScore}.`;
+  resultadoAcum.style.fontSize = "1.5rem"
+  roundResult.appendChild(parrafoRound);
+  roundResult.appendChild(resultadoRound);
+  score.appendChild(resultadoAcum);
+  resultadoMatch.style.fontSize = "1.5rem"
+  if (humanScore === 5) {
+    resultadoMatch.textContent = "You won the match!";
+    matchResult.appendChild(resultadoMatch);
+  } else if (computerScore === 5) {
+    resultadoMatch.textContent = "You lost the match. Computer wins";
+    matchResult.appendChild(resultadoMatch);
+  }
+}
 
+function reset() {
+  humanScore = 0;
+  computerScore = 0;
+  rounds = 0;
+  parrafoRound.remove();
+  resultadoRound.remove();
+  resultadoAcum.remove();
+  resultadoMatch.remove();
+  resetButton.classList.add("hidden")
+}
 
-let x = playGame()
-
-console.log(x);
+// if (humanScore > computerScore) {
+//   return "You won the match!!";
+// } else if (computerScore > humanScore) {
+//   return "Computer wins!!";
+// } else {
+//   return "No one wins. It's a tie!!";
+// }
